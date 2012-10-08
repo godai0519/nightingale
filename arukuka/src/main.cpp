e891bdb1d07d2d36b6e5d5624b3bcea42e11c162
Shingo Ohtsuka <capriciouswalk@gmail.com>
2012-10-08 22:05:40 +0900

    C++11 の std::thread を使って、画像前処理部分を高速化させました。

--- arukuka/src/main.cpp
+++ arukuka/src/main.cpp
@@ -1,10 +1,18 @@
-﻿#include <opencv2/core/core.hpp>
+﻿
+/*
+ * C++11 を使っています、 g++ (GCC) 4.7.1 で。
+ * もしかしたら他の環境ではエラーが出るかもしれません。
+ * g++-4.7 -std=c++11 main.cpp -o main -Wall `pkg-config --libs opencv` `pkg-config --cflags opencv`
+ */
+
+#include <opencv2/core/core.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <iostream>
-#include <cstdio>
-#include <ctime>
+#include <thread>
+#include <vector>
 
+#ifdef _WIN32
 #ifdef _DEBUG
 	#pragma comment(lib,"opencv_core242d.lib")
 	#pragma comment(lib,"opencv_imgproc242d.lib")
@@ -14,6 +22,9 @@
 	#pragma comment(lib,"opencv_imgproc242.lib")
 	#pragma comment(lib,"opencv_highgui242.lib")
 #endif
+#endif
+
+#define INF (1>>28)
 
 cv::Mat src;
 cv::Mat out;
@@ -242,11 +253,11 @@ void draw(){
 	cv::findContours( out, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, cv::Point(0, 0) );
 	
 	// 表示に必要な変数の初期化をします。
-	cv::vector<std::vector<cv::Point> > contours_poly( contours.size() );
+	cv::vector<std::vector<cv::Point>> contours_poly( contours.size() );
 	cv::vector<cv::Rect> boundRect( contours.size() );
 	cv::vector<cv::Point2f>center( contours.size() );
 	cv::vector<float>radius( contours.size() );
-	for( int i = 0; i < contours.size(); i++ ){
+	for( uint i = 0; i < contours.size(); i++ ){
 		// 輪郭線をより直線に近い形に近似します。いらない情報をカットする目的としても良いかもしれません。
 		cv::approxPolyDP( cv::Mat(contours[i]), contours_poly[i], 3, true ); 
 		// 近似された輪郭線を構成する点の傾いていない外接短形を求めます。
@@ -257,23 +268,23 @@ void draw(){
 
 	int count = 0;
 	_map.clear();
-	/// 表示部分です。必要でなさそうな輪郭線(例:半径が小さすぎる)はif文でカットしています。
+	/// 表示部分です。
 	cv::Mat drawing = cv::Mat::zeros( out.size(), CV_8UC3 );
-	for( int i = 0; i< contours.size(); i++ ){ 
+	for( uint i = 0; i< contours.size(); i++ ){ 
 		cv::Scalar color = cv::Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
 		int width = boundRect[i].br().x - boundRect[i].tl().x;
 		int height = boundRect[i].br().y - boundRect[i].tl().y;
-//if((n==0)||(width > height)){ ないほうがいい？
 		cv::rectangle( drawing, boundRect[i].tl(), boundRect[i].br(), color, 2, 8, 0 );
 		cv::circle( drawing, center[i], (int)radius[i], color, 2, CV_AA);
 		++count;
+		if(n==0){width>>=1; height>>=1;}
+		std::cout << width << ", " << height << std::endl;
 		int S = width*height;
 		if(_map.find(S)!=_map.end()){
 			_map[S] = _map[S]+1;
 		}else{
 			_map[S] = 1;
 		}
-//}
 	}
 	cv::namedWindow( "Contours", CV_WINDOW_NORMAL );
 	cv::imshow( "Contours", drawing );
@@ -282,7 +293,7 @@ void draw(){
 	std::cout << "count = " << (n==0 ? count : (int)(count/3.5 + .5)) << std::endl;
 }
 
-void sharp(const cv::Mat src, cv::Mat& dst, float k = 2.0f)
+void sharp(cv::Mat const& src, cv::Mat& dst, float k = 2.0f)
 {
 	float kernelData[] = {
 		-k/9.0f, -k/9.0f, -k/9.0f,
@@ -293,34 +304,66 @@ void sharp(const cv::Mat src, cv::Mat& dst, float k = 2.0f)
 	cv::filter2D(src, dst, -1, kernel);
 }
 
-
-void gamma(const cv::Mat src, cv::Mat& dst, double gamma = 2.0)
+void _gamma(cv::Mat const& src, cv::Mat& dst, unsigned char const LUT[256], int const S[2], int const E[2])
 {
-	static bool f = true;
-	int i;   
-	static unsigned char LUT[256];   
-	
-	if(f){
-		f = false;         
-		//ガンマ補正テーブルの作成   
-		for (i = 0; i < 256; i++){   
-			LUT[i] = (int)(pow((double)i / 255.0, 1.0 / gamma) * 255.0);   
-		}
-	}
-	for(int y=0; y<src.rows; ++y){
-		for(int x=0; x<src.cols; ++x){
+	for(int y=S[1]; y<E[1]; ++y){
+		for(int x=S[0]; x<E[0]; ++x){
 			const cv::Vec3b p = src.at<cv::Vec3b>(y,x);
 			dst.at<cv::Vec3b>(y,x) = cv::Vec3b(LUT[p[0]], LUT[p[1]], LUT[p[2]]);
 		}
 	}
 }
 
-void normalise(const cv::Mat src, cv::Mat& dst)
+void gamma(cv::Mat const& src, cv::Mat& dst, double gamma = 2.0)
+{
+	static bool f = true;
+	static unsigned char LUT[256];   
+	
+	if(f){
+		f = false;         
+		//ガンマ補正テーブルの作成   
+		for (int i = 0; i < 256; i++){   
+			LUT[i] = (int)(pow((double)i / 255.0, 1.0 / gamma) * 255.0);   
+		}
+	}
+	
+	int p[2][2][2][2];	// アドレスが変わらないように...
+	std::vector<std::thread> th;
+	for(int y=0; y<2; ++y){
+		for(int x=0; x<2; ++x){
+			int S[2] = {(src.cols/2)*x, (src.rows/2)*y};
+			int E[2] = {(src.cols/2)*(x+1), (src.rows/2)*(y+1)};
+			p[y][x][0][0] = S[0];  p[y][x][0][1] = S[1];
+			p[y][x][1][0] = E[0];  p[y][x][1][1] = E[1];
+			th.push_back(std::thread(std::bind(_gamma, src, std::ref(dst), LUT, p[y][x][0], p[y][x][1])));
+		}
+	}
+	for(auto& i : th) i.join();
+}
+
+void normalise(cv::Mat const& src, cv::Mat& dst)
 {
 	double max = 0., min = 0.;
 	cv::minMaxLoc(src, &min, &max);
 	cv::convertScaleAbs(src, dst, 255.0/(max-min), (255.0/(max-min))*(-min));
 }
+
+void pre_pros_red(cv::Mat const& src, cv::Mat &dst, cv::Mat &red){
+	cv::medianBlur(src, dst, 13);
+	normalise(dst, dst);
+	
+	cv::Mat hsv;
+	cv::cvtColor(dst, hsv, CV_BGR2HSV);
+	red_area(red,hsv);
+}
+void pre_pros_black(cv::Mat const& src, cv::Mat &dst, cv::Mat &black){
+	cv::dilate(src, dst, cv::Mat());
+	normalise(dst, dst);
+	sharp(dst, dst, 6.4f);
+	
+	black_area(black,dst);
+}
+
 
 int main(int argc, char** argv)
 {
@@ -331,7 +374,9 @@ int main(int argc, char** argv)
 		for(int num=1; num<argc; ++num){
 			// 画像の読み込み
 			src = cv::imread(argv[num], 1);
+			cv::resize(src, src, cv::Size(1200, 900));
 			gamma(src, src);			// 共通前処理
+			
 		
 			// ウィンドウ表示部分の初期化です。
 			std::string source_window = "Source";
@@ -344,18 +389,14 @@ int main(int argc, char** argv)
 			// Result については表示したときに逐一設定します。
 
 
-			// 赤黒別前処理
-			cv::Mat dst[2];
-			cv::medianBlur(src, dst[0], 13);
-			normalise(dst[0], dst[0]);
-			sharp(src, dst[1], 3.2f);
-
 			// 赤と黒を取り出す
-			cv::Mat red,black,hsv;
-			cv::cvtColor(dst[0], hsv, CV_BGR2HSV);
-			red_area(red,hsv);
-			black_area(black,dst[1]);
-	
+			cv::Mat dst[2];
+			cv::Mat red,black;
+			std::thread th_red(std::bind(pre_pros_red, src, std::ref(dst[0]), std::ref(red)));
+			std::thread th_black(std::bind(pre_pros_black, src, std::ref(dst[1]), std::ref(black)));
+
+			th_red.join();
+			th_black.join();
 			cv::Mat img[2] = {red.clone(), black.clone()};
 
 			for(int i=0; i<2; ++i){
@@ -365,7 +406,6 @@ int main(int argc, char** argv)
 				for(char inputkey = 0; inputkey != 'q';inputkey = cv::waitKey(0)); //qが入力されるまで待機
 				//_map -> ans 写
 				std::map<int,int>::iterator it = _map.begin();
-				int buf = 0;
 				cv::Mat drawing = cv::Mat::zeros( cv::Size(1000,300), CV_8UC3 );
 				while(it!=_map.end()){
 					cv::Scalar color = cv::Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
@@ -406,7 +446,17 @@ int main(int argc, char** argv)
 		std::cout << "Answer..." << ans[0] << ", " << ans[1] << ", " << ans[2] << std::endl;
 		std::time(&stop);
 		std::cout << "Time..." << std::difftime(stop, start) << std::endl;
+		
+		double g=INF;
+		std::cin >> g;
+		g -= ans[2]*5.902;
+		g -= ans[1]*1.35;
+		int _small = (int)(g/0.274 + 0.5);
+		ans[0] = std::min(ans[0], _small);	// ノイズ混入が考えられるのでminで
+		
+		std::cout << "Answer..." << ans[0] << ", " << ans[1] << ", " << ans[2] << std::endl;
 	}
 	
 	return 0;
 }
+
